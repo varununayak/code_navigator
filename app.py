c631bf0e1baddc1760e96d71a6a9d9e1a9aad4fa
Varun Nayak <vnayak@dexterity.ai>
2024-02-19 17:32:31 -0800

    class

--- app.py
+++ app.py
@@ -9,41 +9,39 @@ from llama_index.core import set_global_tokenizer, ServiceContext, SimpleDirecto
 _MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v0.6"
 _CODE_FILES = glob.glob(str(Path("/home/varun/personal/code_navigator/data/") / '*'), recursive=True)
 _QUERY = "What does the python function 'foo' do?"
+PERSIST_DIR = "./storage"
 
 OPEN_AI_KEY = "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"
 
 os.environ['OPENAI_API_KEY'] = OPEN_AI_KEY
 
-# check if storage already exists
-PERSIST_DIR = "./storage"
-if not os.path.exists(PERSIST_DIR):
-    print("Creating Index")
-    # load the documents and create the index
-    documents = SimpleDirectoryReader(input_files=_CODE_FILES).load_data()
-    index = VectorStoreIndex.from_documents(documents)
-    # store it for later
-    index.storage_context.persist(persist_dir=PERSIST_DIR)
-else:
-    # load the existing index
-    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-    index = load_index_from_storage(storage_context)
-    print("Using stored index")
+class ChatBot:
 
 
-documents = SimpleDirectoryReader(input_files=_CODE_FILES).load_data()
-index = VectorStoreIndex.from_documents(documents)
+    def __init__(self):
+        # check if storage already exists
+        if not os.path.exists(PERSIST_DIR):
+            print("Creating Index")
+            # load the documents and create the index
+            documents = SimpleDirectoryReader(input_files=_CODE_FILES).load_data()
+            index = VectorStoreIndex.from_documents(documents)
+            # store it for later
+            index.storage_context.persist(persist_dir=PERSIST_DIR)
+        else:
+            # load the existing index
+            storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+            index = load_index_from_storage(storage_context)
+            print("Using stored index")
 
-query_engine = index.as_chat_engine()
 
-response = query_engine.chat(_QUERY)
+        documents = SimpleDirectoryReader(input_files=_CODE_FILES).load_data()
+        self.index = VectorStoreIndex.from_documents(documents)
 
-print(response)
 
-response = query_engine.chat("Can you give me an example usage in python?")
+    def chat(self, message: str, history: str):
+        query_engine = self.index.as_chat_engine()
+        response = query_engine.chat(message)
+        return str(response)
 
-print(response)
-
-# def chat(message: str, history: str):
-#     return f"Message: {message}. History {history}"
-
-# gr.ChatInterface(chat).launch()
+chatbot = ChatBot()
+gr.ChatInterface(chatbot.chat).launch()
