50fd6acf661634e24737b4e87af8d7898a3d1e1b
Varun Nayak <vnayak@dexterity.ai>
2024-02-23 16:10:37 -0800

    wip

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ from pathlib import Path
 
 import gradio as gr
 import os
+
+import torch
 from llama_index.core import set_global_tokenizer, ServiceContext, SimpleDirectoryReader, VectorStoreIndex, Settings, \
     StorageContext, load_index_from_storage, PromptTemplate
 from llama_index.legacy.llms import HuggingFaceLLM
+from llama_index.core.callbacks.base import CallbackManager
 
-_MODEL_NAME = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+from accelerate.utils import BnbQuantizationConfig
+
+
+assert (torch.cuda.is_available())
+_MODEL_NAME = "microsoft/phi-2"
 _CODE_FILES = glob.glob(str(Path("/home/varun/personal/code_navigator/data/") / '*'), recursive=True)
 _QUERY = "What does the python function 'foo' do?"
 PERSIST_DIR = "./storage"
@@ -20,24 +27,28 @@ class ChatBot:
 
 
     def __init__(self):
-        query_wrapper_prompt = PromptTemplate(
-            "Below is an instruction that describes a task. "
-            "Write a response that appropriately completes the request.\n\n"
-            "### Instruction:\n{query_str}\n\n### Response:"
+        bnb_quantization_config = BnbQuantizationConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4"
         )
+        Settings.callback_manager = CallbackManager()
+        Settings.chunk_size = 512
         llm = HuggingFaceLLM(
             context_window=2048,
             max_new_tokens=256,
-            generate_kwargs={"temperature": 0.25, "do_sample": False},
+            generate_kwargs={"temperature": 0.1, "do_sample": True},
             tokenizer_name=_MODEL_NAME,
             model_name=_MODEL_NAME,
             device_map="auto",
             tokenizer_kwargs={"max_length": 2048},
-            # query_wrapper_prompt=query_wrapper_prompt,
+            # quantization_config=bnb_quantization_config,
+            #query_wrapper_prompt=query_wrapper_prompt,
             # uncomment this if using CUDA to reduce memory usage
-            # model_kwargs={"torch_dtype": torch.float16}
+            model_kwargs={
+            "torch_dtype": torch.float16}
         )
-        Settings.chunk_size = 512
         Settings.llm = llm
 
         # load the documents and create the index
@@ -46,12 +57,12 @@ class ChatBot:
         # store it for later
         index.storage_context.persist(persist_dir=PERSIST_DIR)
 
-        self.query_engine = index.as_chat_engine(llm=llm)
+        self.query_engine = index.as_query_engine(llm=llm)
 
     def chat(self, message: str, history: str = None):
-        response = self.query_engine.chat(message)
+        response = self.query_engine.query(message)
         return str(response)
 
 chatbot = ChatBot()
-print(chatbot.chat("Hello"))
-gr.ChatInterface(chatbot.chat).launch()
+print(f"The answer is: {chatbot.chat('What does the function foo do?')}")
+#gr.ChatInterface(chatbot.chat).launch()
